bea5fb550010a103627314061cce4c9795b4b900
Anderson Pablo <mib_anderson@hotmail.com>
2016-09-28 16:02:33 -0300

    contador

--- ethernet.ino
+++ ethernet.ino
@@ -1,32 +1,63 @@
 #include <UIPEthernet.h>
-
+ 
 EthernetServer server = EthernetServer(80);
 
+int sensor1 = 8;
+int sensor2 = 9;
+unsigned long timeS1 = 0, timeS2 = 0;
+unsigned long dif;
+long int nPessoas = 0;
+
+ 
 void setup()
 {
- Serial.begin(9600);
+	pinMode(sensor1, INPUT);
+	pinMode(sensor2, INPUT);
 
- uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x04,0x05};
- IPAddress myIP(192,168,0,25);
- Ethernet.begin(mac,myIP);
+	Serial.begin(9600);
 
- server.begin();
+	uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x04,0x05};
+	IPAddress myIP(192,168,0,25);
+	Ethernet.begin(mac,myIP);
+	server.begin();
 }
-
+ 
 void loop()
 {
- size_t size;
+	if (!digitalRead(sensor1))
+	{
+    	timeS1 = millis(); 
+    }
+    if (!digitalRead(sensor2))
+    {
+    	timeS2 = millis(); 
+    }
 
- if (EthernetClient client = server.available())
- {
- while((size = client.available()) > 0)
- {
- uint8_t* msg = (uint8_t*)malloc(size);
- size = client.read(msg,size);
- Serial.write(msg,size);
- free(msg);
- }
- client.println("<h1>Hello World!</h1>");
- client.stop();
- }
+    dif = timeS2 - timeS1;
+
+    dif = timeS2 - timeS1;
+    if(dif >= 500 && dif <= 1500)
+    {
+       timeS1 = timeS2 = 0;
+       nPessoas++;
+   }
+
+	size_t size;
+ 
+	if (EthernetClient client = server.available())
+	{
+ 		while((size = client.available()) > 0)
+ 		{
+			uint8_t* msg = (uint8_t*)malloc(size);
+			size = client.read(msg,size);
+			Serial.write(msg,size);
+			free(msg);
+		}
+		client.println("<h1>Hello World!</h1>");
+		client.println(nPessoas);
+		client.stop();
+	}
+
+	delay(10);
+
 }
