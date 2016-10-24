a59db434d6d2ba3bce251349126f1efedb7e16b8
Tyrone Damasceno <tyronedamasceno@gmail.com>
2016-10-24 08:59:07 -0300

    Leitura sensores

--- arduino_webclient/arduino_webclient.ino
+++ arduino_webclient/arduino_webclient.ino
@@ -7,31 +7,57 @@ int sensor1 = 5;
 int sensor2 = 10;
 String data;
 
+int limiteInf = 500, limiteSup = 1500;
+unsigned long timeS1 = 0, timeS2 = 0;
+unsigned long dif;
+long int nPessoas = 0;
+
 void setup() { 
+
+	pinMode(sensor1, INPUT);
+	pinMode(sensor2, INPUT);
 
   	Serial.begin(9600);
 
-	if (Ethernet.begin(mac) == 0) {
+	if (Ethernet.begin(mac) == 0)
 		Serial.println("Falha na configuração DHCP"); 
-	} else {
-                Serial.println("ok");
-}
+	else
+        Serial.println("ok");
   
   data = "";
 }
 
 void loop(){
   
-String data = "sensor1=" + String(sensor1) + "&sensor2=" + String(sensor2);        
+	if (!digitalRead(sensor1))
+	{
+    	timeS1 = millis(); 
+    }
+    if (!digitalRead(sensor2))
+    {
+    	timeS2 = millis(); 
+    }
+
+    dif = timeS2 - timeS1;
+
+    if (dif >= limiteInf and dif <= limiteSup)
+    {
+    	timeS1 = timeS2 = 0;
+    	nPessoas++;
+    }
+
+
+	String data = "sensor1=" + String(sensor1) + "&sensor2=" + String(sensor2) + "\nNumero de pessoas: " + String(nPessoas);        
 
 	if (client.connect("192.168.0.101",80)) {
-                Serial.println("-> Conectado");
+    	Serial.println("-> Conectado");
 		client.print("POST /sensor/add.php?"+String(data));
-            	client.println(); 
-                client.stop();
-} 
+    	client.println(); 
+        client.stop();
+	}
+
 	if (client.connected()) { 
 		client.stop();
 	}
-	delay(1000);
+	delay(100);
 }
